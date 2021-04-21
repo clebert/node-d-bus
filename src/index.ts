d0a719e3fed15b0bca04f4496a80864882380a16
Clemens Akens <clebert@me.com>
2021-04-22 01:55:07 +0200

    Make `TextDecoder` and `TextEncoder` globally available if necessary

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import {TextDecoder, TextEncoder} from 'util';
+
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = TextDecoder as any;
+}
+
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = TextEncoder as any;
+}
+
 export * from './d-bus';
 export * from './system-d-bus';
